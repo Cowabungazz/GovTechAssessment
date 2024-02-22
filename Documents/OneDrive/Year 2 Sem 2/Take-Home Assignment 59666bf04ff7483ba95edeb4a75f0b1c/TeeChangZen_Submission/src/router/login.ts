5e79a351188faa5922d57adc2809a695d68259fc
Tee Chang Zen <107359897+Cowabungazz@users.noreply.github.com>
2024-02-23 03:22:19 +0800

    Delete Documents/OneDrive/Year 2 Sem 2/Take-Home Assignment 59666bf04ff7483ba95edeb4a75f0b1c/TeeChangZen_Submission directory

--- Documents/OneDrive/Year 2 Sem 2/Take-Home Assignment 59666bf04ff7483ba95edeb4a75f0b1c/TeeChangZen_Submission/src/router/login.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { login } from '../controllers/login';
-
-
-export default (router: express.Router) => {
-    router.post('/login', login); //router is used to handle the request and response
-    return router;
-}
